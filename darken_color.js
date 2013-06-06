2d1e628d30490b6e6346ccc6f7eecfaef2ac53b8
Artem Shoobovych <ars@qapint.com>
2013-06-06 12:15:15 +0300

    Initial  commit

---
+++ darken_color.js
@@ -0,0 +1,314 @@
+var COLORS = {
+  aliceblue: [240, 248, 255]
+  , antiquewhite: [250, 235, 215]
+  , aqua: [0, 255, 255]
+  , aquamarine: [127, 255, 212]
+  , azure: [240, 255, 255]
+  , beige: [245, 245, 220]
+  , bisque: [255, 228, 196]
+  , black: [0, 0, 0]
+  , blanchedalmond: [255, 235, 205]
+  , blue: [0, 0, 255]
+  , blueviolet: [138, 43, 226]
+  , brown: [165, 42, 42]
+  , burlywood: [222, 184, 135]
+  , cadetblue: [95, 158, 160]
+  , chartreuse: [127, 255, 0]
+  , chocolate: [210, 105, 30]
+  , coral: [255, 127, 80]
+  , cornflowerblue: [100, 149, 237]
+  , cornsilk: [255, 248, 220]
+  , crimson: [220, 20, 60]
+  , cyan: [0, 255, 255]
+  , darkblue: [0, 0, 139]
+  , darkcyan: [0, 139, 139]
+  , darkgoldenrod: [184, 132, 11]
+  , darkgray: [169, 169, 169]
+  , darkgreen: [0, 100, 0]
+  , darkgrey: [169, 169, 169]
+  , darkkhaki: [189, 183, 107]
+  , darkmagenta: [139, 0, 139]
+  , darkolivegreen: [85, 107, 47]
+  , darkorange: [255, 140, 0]
+  , darkorchid: [153, 50, 204]
+  , darkred: [139, 0, 0]
+  , darksalmon: [233, 150, 122]
+  , darkseagreen: [143, 188, 143]
+  , darkslateblue: [72, 61, 139]
+  , darkslategray: [47, 79, 79]
+  , darkslategrey: [47, 79, 79]
+  , darkturquoise: [0, 206, 209]
+  , darkviolet: [148, 0, 211]
+  , deeppink: [255, 20, 147]
+  , deepskyblue: [0, 191, 255]
+  , dimgray: [105, 105, 105]
+  , dimgrey: [105, 105, 105]
+  , dodgerblue: [30, 144, 255]
+  , firebrick: [178, 34, 34]
+  , floralwhite: [255, 255, 240]
+  , forestgreen: [34, 139, 34]
+  , fuchsia: [255, 0, 255]
+  , gainsboro: [220, 220, 220]
+  , ghostwhite: [248, 248, 255]
+  , gold: [255, 215, 0]
+  , goldenrod: [218, 165, 32]
+  , gray: [128, 128, 128]
+  , green: [0, 128, 0]
+  , greenyellow: [173, 255, 47]
+  , grey: [128, 128, 128]
+  , honeydew: [240, 255, 240]
+  , hotpink: [255, 105, 180]
+  , indianred: [205, 92, 92]
+  , indigo: [75, 0, 130]
+  , ivory: [255, 255, 240]
+  , khaki: [240, 230, 140]
+  , lavender: [230, 230, 250]
+  , lavenderblush: [255, 240, 245]
+  , lawngreen: [124, 252, 0]
+  , lemonchiffon: [255, 250, 205]
+  , lightblue: [173, 216, 230]
+  , lightcoral: [240, 128, 128]
+  , lightcyan: [224, 255, 255]
+  , lightgoldenrodyellow: [250, 250, 210]
+  , lightgray: [211, 211, 211]
+  , lightgreen: [144, 238, 144]
+  , lightgrey: [211, 211, 211]
+  , lightpink: [255, 182, 193]
+  , lightsalmon: [255, 160, 122]
+  , lightseagreen: [32, 178, 170]
+  , lightskyblue: [135, 206, 250]
+  , lightslategray: [119, 136, 153]
+  , lightslategrey: [119, 136, 153]
+  , lightsteelblue: [176, 196, 222]
+  , lightyellow: [255, 255, 224]
+  , lime: [0, 255, 0]
+  , limegreen: [50, 205, 50]
+  , linen: [250, 240, 230]
+  , magenta: [255, 0, 255]
+  , maroon: [128, 0, 0]
+  , mediumaquamarine: [102, 205, 170]
+  , mediumblue: [0, 0, 205]
+  , mediumorchid: [186, 85, 211]
+  , mediumpurple: [147, 112, 219]
+  , mediumseagreen: [60, 179, 113]
+  , mediumslateblue: [123, 104, 238]
+  , mediumspringgreen: [0, 250, 154]
+  , mediumturquoise: [72, 209, 204]
+  , mediumvioletred: [199, 21, 133]
+  , midnightblue: [25, 25, 112]
+  , mintcream: [245, 255, 250]
+  , mistyrose: [255, 228, 225]
+  , moccasin: [255, 228, 181]
+  , navajowhite: [255, 222, 173]
+  , navy: [0, 0, 128]
+  , oldlace: [253, 245, 230]
+  , olive: [128, 128, 0]
+  , olivedrab: [107, 142, 35]
+  , orange: [255, 165, 0]
+  , orangered: [255, 69, 0]
+  , orchid: [218, 112, 214]
+  , palegoldenrod: [238, 232, 170]
+  , palegreen: [152, 251, 152]
+  , paleturquoise: [175, 238, 238]
+  , palevioletred: [219, 112, 147]
+  , papayawhip: [255, 239, 213]
+  , peachpuff: [255, 218, 185]
+  , peru: [205, 133, 63]
+  , pink: [255, 192, 203]
+  , plum: [221, 160, 203]
+  , powderblue: [176, 224, 230]
+  , purple: [128, 0, 128]
+  , red: [255, 0, 0]
+  , rosybrown: [188, 143, 143]
+  , royalblue: [65, 105, 225]
+  , saddlebrown: [139, 69, 19]
+  , salmon: [250, 128, 114]
+  , sandybrown: [244, 164, 96]
+  , seagreen: [46, 139, 87]
+  , seashell: [255, 245, 238]
+  , sienna: [160, 82, 45]
+  , silver: [192, 192, 192]
+  , skyblue: [135, 206, 235]
+  , slateblue: [106, 90, 205]
+  , slategray: [119, 128, 144]
+  , slategrey: [119, 128, 144]
+  , snow: [255, 255, 250]
+  , springgreen: [0, 255, 127]
+  , steelblue: [70, 130, 180]
+  , tan: [210, 180, 140]
+  , teal: [0, 128, 128]
+  , thistle: [216, 191, 216]
+  , tomato: [255, 99, 71]
+  , turquoise: [64, 224, 208]
+  , violet: [238, 130, 238]
+  , wheat: [245, 222, 179]
+  , white: [255, 255, 255]
+  , whitesmoke: [245, 245, 245]
+  , yellow: [255, 255, 0]
+  , yellowgreen: [154, 205, 5]
+};
+
+function RGB2HSL(r, g, b) {
+  var h, // 0..360
+      l, s; // 0..1
+
+  // 0..1 に変換
+  r = r / 255;
+  g = g / 255;
+  b = b / 255;
+  var max = Math.max(Math.max(r, g), b),
+      min = Math.min(Math.min(r, g), b);
+
+  // hue の計算
+  if (max == min) {
+    h = 0; // 本来は定義されないが、仮に0を代入
+  } else if (max == r) {
+    h = 60 * (g - b) / (max - min) + 0;
+  } else if (max == g) {
+    h = (60 * (b - r) / (max - min)) + 120;
+  } else {
+    h = (60 * (r - g) / (max - min)) + 240;
+  }
+
+  while (h < 0) {
+    h += 360;
+  }
+
+  // Lightness の計算
+  l = (max + min) / 2;
+
+  // Saturation の計算
+  if (max == min) {
+    s = 0;
+  } else {
+    s = (l < 0.5)
+      ? (max - min) / (max + min)
+      : (max - min) / (2.0 - max - min);
+  }
+
+  return {'h': h, 'l': l, 's': s, 'type': 'HSL'};
+}
+
+function HSL2RGB(h, s, l) {
+  // helper function used above
+    function hueToRgb(l1, l2, h) {
+        if (h < 0) h += 1;
+        if (h > 1) h -= 1;
+
+        if (h < 1 / 6) return (l1 + (l2 - l1) * 6 * h);
+        if (h < 1 / 2) return l2;
+        if (h < 2 / 3) return (l1 + (l2 - l1) * ((2 / 3) - h) * 6);
+
+        return l1;
+    }
+
+    h /= 360; s /= 100; l /= 100;
+
+    var r, g, b;
+
+    if (s == 0) {
+        r = g = b = l;
+    } else {
+        var l2 = l < 0.5 ? l * (1 + s) : (l + s) - (s * l);
+        var l1 = (2 * l) - l2;
+
+        r = hueToRgb(l1, l2, (h + (1 / 3)));
+        g = hueToRgb(l1, l2, h);
+        b = hueToRgb(l1, l2, (h - (1 / 3)));
+    }
+
+    r = Math.round(255 * r);
+    g = Math.round(255 * g);
+    b = Math.round(255 * b);
+
+    return { 'r': r, 'g': g, 'b': b, 'type': 'RGB' };
+}
+
+function HEX2RGB(hex) {
+    // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
+    var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+
+    hex = hex.replace(shorthandRegex, function(m, r, g, b) {
+        return r + r + g + g + b + b;
+    });
+
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    
+    return result ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16)
+    } : null;
+}
+
+function RGB2HEX(r, g, b) {
+    return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+}
+
+function NAME2RGB(name) {
+  var color = COLORS[name] || [0, 0, 0];
+
+  return { r: color[0], g: color[1], b: color[2] };
+}
+
+function darken(color, amount) {
+  var r_g_b;
+
+  if (/^#/.test(color)) {
+    r_g_b = HEX2RGB(color);
+  } else {
+    r_g_b = NAME2RGB(color);
+  }
+
+  console.log('parsed color: ', r_g_b);
+  
+  var h_s_l = RGB2HSL(r_g_b.r, r_g_b.g, r_g_b.b);
+
+  console.log('HSL color: ', h_s_l);
+
+  if (/^\d+(%)$/.test(amount)) {
+    amount = {
+      type: '%',
+      val: parseFloat(amount.replace(/(%)$/, ''))
+    };
+  } else if (/^\d+\.\d+$/.test(amount)) {
+    amount = {
+      type: 'f',
+      val: parseFloat(amount) * 100.0
+    }
+  } else {
+    amount = {
+      type: 'i',
+      val: parseFloat(amount)
+    }
+  }
+
+  console.log('parsed amount: ', amount);
+
+  var val = amount.val;
+
+  if ('%' == amount.type) {
+    val = val > 0
+      ? (100 - h_s_l['l']) * val / 100
+      : h_s_l['l'] * (val / 100);
+  }
+
+  console.log('resulting HSL #1: ', h_s_l);
+
+  h_s_l['l'] += val;
+
+  console.log('resulting HSL #2: ', h_s_l);
+
+  r_g_b = HSL2RGB(h_s_l);
+
+  console.log('resulting RGB: ', r_g_b);
+
+  // return 'rgb(' + r_g_b.r + ', ' + r_g_b.g + ', ' + r_g_b.b + ')';
+
+  return RGB2HEX(r_g_b.r, r_g_b.g, r_g_b.b);
+}
+
+console.log(darken('red', '0.7'));
+console.log(darken('red', '70'));
+console.log(darken('#f00', '70'));
+console.log(darken('#ff0000', '70%'));
