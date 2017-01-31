fa0708e36629fd785bb835d04dea9879b44b251d
Artem Shubovych <shybovycha@gmail.com>
2017-01-31 13:37:11 +0100

    Added tests, support for browser and node env and some code style

--- README.md
+++ README.md
@@ -1,12 +1,15 @@
-darken_color.js
-===============
+# darken_color.js
 
-Provide methods to parse color names/values and lightens or darkens it by passed amount value.
+Provide methods to parse color names/values and lightens or darkens it by passed amount value. Supports both Node and browser environment.
 
-## Usage examples ##
+## Usage examples
 
-    darken('lightgreen', '0.1'); // -> #63e763 
-    darken('lightgreen', '10'); // -> #63e763
-    darken('#90EE90', '10'); // -> #63e763
-    darken('#9e9', '10%'); // ->  #98ed98
+    darken('lightgreen', '0.1'); // -> #bcf4bc
+    darken('lightgreen', '10'); // -> #bcf4bc
+    darken('#90EE90', '10'); // -> #bcf4bc
+    darken('#9e9', '10%'); // ->  #c4f5c4
+    lighten('lightgreen', 10); // -> #bcf4bc
 
+## Tests
+
+Run `npm install && npm run-script test` in order to run Jasmine tests.
---
+++ darken_color_spec.js
@@ -0,0 +1,37 @@
+var darken_color = require('./darken_color');
+
+describe("darken_color", function () {
+    it('works with word color names', function () {
+	expect(darken_color.darken('lightgreen', 0.1)).toEqual('#63e763');
+    });
+
+    it('works with 6-digit colors', function () {
+	expect(darken_color.darken('#90EE90', 0.1)).toEqual('#63e763');
+    });
+
+    it('works with 3-digit colors', function () {
+	expect(darken_color.darken('#9E9', 0.1)).toEqual('#6de66d');
+    });
+
+    it('accepts percentage amount', function () {
+	expect(darken_color.darken('lightgreen', '10%')).toEqual('#8fed8f');
+    });
+
+    it('accepts floating-point amount', function () {
+	expect(darken_color.darken('lightgreen', 0.1)).toEqual('#63e763');
+    });
+
+    it('accepts integer amount', function () {
+	expect(darken_color.darken('lightgreen', 10)).toEqual('#63e763');
+    });
+
+    it('lightens colors', function () {
+	expect(darken_color.lighten('lightgreen', 10)).toEqual('#bcf4bc');
+    });
+
+    it('handles incorrect color formats', function () {
+	expect(function () {
+	    darken_color.darken('lightgreen', 'Kickapoo')
+	}).toThrow();
+    });
+});
