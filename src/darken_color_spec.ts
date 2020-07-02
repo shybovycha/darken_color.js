77d1ff07221348204fceb5dd4de277272e99c7ce
Artem Shubovych <ashubovych@atlassian.com>
2020-07-02 16:30:02 +1000

    Migrate to TS

--- src/darken_color_spec.ts
+++ src/darken_color_spec.ts
@@ -1,16 +1,16 @@
-const darken_color = require("./darken_color");
+import * as darken_color from "./darken_color";
 
 describe("darken_color", () => {
   it("works with word color names", () => {
-    expect(darken_color.darken("lightgreen", 0.1)).toBe("#63e763");
+    expect(darken_color.darken("lightgreen", "0.1")).toBe("#63e763");
   });
 
   it("works with 6-digit colors", () => {
-    expect(darken_color.darken("#90EE90", 0.1)).toBe("#63e763");
+    expect(darken_color.darken("#90EE90", "0.1")).toBe("#63e763");
   });
 
   it("works with 3-digit colors", () => {
-    expect(darken_color.darken("#9E9", 0.1)).toBe("#6de66d");
+    expect(darken_color.darken("#9E9", "0.1")).toBe("#6de66d");
   });
 
   it("accepts percentage amount", () => {
@@ -18,15 +18,15 @@ describe("darken_color", () => {
   });
 
   it("accepts floating-point amount", () => {
-    expect(darken_color.darken("lightgreen", 0.1)).toBe("#63e763");
+    expect(darken_color.darken("lightgreen", "0.1")).toBe("#63e763");
   });
 
   it("accepts integer amount", () => {
-    expect(darken_color.darken("lightgreen", 10)).toBe("#63e763");
+    expect(darken_color.darken("lightgreen", "10")).toBe("#63e763");
   });
 
   it("lightens colors", () => {
-    expect(darken_color.lighten("lightgreen", 10)).toBe("#bcf4bc");
+    expect(darken_color.lighten("lightgreen", "10")).toBe("#bcf4bc");
   });
 
   it("handles incorrect color formats", () => {
